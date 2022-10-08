608097d341ce7b998376c7d3acb598442bffcd50
SarimEffendi <sarimeffendi2020@gmail.com>
2022-10-08 13:54:50 +0500

    more tasks

---
+++ q21.js
@@ -0,0 +1,10 @@
+//They think of something you could store in a JavaScript Object. Write a program that creates Objects containing these items.
+
+const person = {
+  firstName: "Muhammad",
+  lastName: "Sarim",
+  age: 20,
+  nationality: "Pakistani",
+  major: "metaverse",
+};
+console.log(`I am ${person.firstName} ${person.lastName} ${person.age} years old and I am from ${person.nationality} and studying ${person.major}. `);
---
+++ q22.js
@@ -0,0 +1,9 @@
+//Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+
+const array=["web2","web3","metaverse","Ai","App developement"];
+console.log("error:");
+//will print undefined because index is not present in array
+console.log(array[-1]);
+console.log("corrected:")
+//normal array
+console.log(array[4]);
