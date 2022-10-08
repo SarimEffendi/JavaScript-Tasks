36b4da8858b66d1d3019c451010a05d7189e0775
SarimEffendi <sarimeffendi2020@gmail.com>
2022-10-08 20:08:13 +0500

    all tasks completed

--- q24.js
+++ q24.js
@@ -12,3 +12,85 @@
 • Test whether an item is not in a array
  */
 
+//Equality and Inequality string
+var str = "commit"
+console.log("Is str == commit? I predict True.")
+console.log(str == "commit")
+console.log("Is str == Commit? I predict False.")
+console.log(str == "Commit")
+
+//Lowercase
+var value="Cycle"
+console.log("Is lower case value == cycle? I predict True.")
+console.log(value.toLowerCase() == "cycle")
+console.log("Is lowercase value == Cycle I predict False.")
+console.log(value.toLowerCase == "Cycle")
+
+//number check
+var num = 12
+console.log("Is num == 12? I predict True.")
+console.log(num==12)
+
+console.log("Is num == 8? I predict False.")
+console.log(num==8)
+
+console.log("Is num != 16? I predict True.")
+console.log(num!=16)
+
+console.log("Is num != 12? I predict False.")
+console.log(num != 12)
+
+console.log("Is num < 13? I predict True.")
+console.log(num < 13)
+
+console.log("Is num < 12? I predict False.")
+console.log(num < 12)
+
+console.log("Is num > 3? I predict True.")
+console.log(num > 3)
+console.log("Is num > 12? I predict False.")
+console.log(num > 12)
+
+console.log("Is num <= 12? I predict True.")
+console.log(num <= 12)
+
+console.log("Is num <= 2? I predict False.")
+console.log(num <= 2)
+
+console.log("Is num >= 12? I predict True.")
+console.log(num <= 12)
+
+console.log("Is num >= 18? I predict False.")
+console.log(num >= 18)
+
+//And Or
+var valNum = 7
+var valStr = "clock"
+
+console.log("Is valNum==7 and valStr==clock? I predict True.")
+console.log(valNum==7 && valStr=="clock")
+
+console.log("Is valNum==7 and valStr!=clock? I predict False.")
+console.log(valNum==7 && valStr!="clock")
+
+console.log("Is valNum!=7 or valStr==clock? I predict True.")
+console.log(valNum!=7 || valStr=="clock")
+
+console.log("Is valNum!=7 and valStr!=clock? I predict False.")
+console.log(valNum!=7 && valStr!="clock")
+
+//In array
+var testArr = [1,2,5,7]
+
+console.log("Is number 7 in testArr. I predict True.")
+console.log(testArr.includes(7))
+
+console.log("Is number 8 in testArr. I predict False.")
+console.log(testArr.includes(8))
+
+//Not in array
+console.log("Is number 6 not in testArr. I predict True.")
+console.log(!testArr.includes(6))
+
+console.log("Is number 2 not in testArr. I predict False.")
+console.log(!testArr.includes(2))
--- q45.js
+++ q45.js
@@ -5,10 +5,56 @@ Call the function with the required information and two other name-value pairs,
 such as a color or an optional feature. Print the Object that’s returned to make sure
 all the information was stored correctly.
  * 
- */ 
+ */
 
-function car_info(){
-    var information= {
-        
-    }
+//was unsure about this question so i used two approaches for this question
+function car_info1(
+  manufacturer,
+  model,
+  color = " ",
+  airbags = true,
+  number_of_airbags
+) {
+  var information = {
+    manufacturer: manufacturer,
+    model: model,
+    color: color,
+    airbags: airbags,
+    number_of_airbags: number_of_airbags,
+  };
+  return information;
 }
+var car1 = car_info1("Toyota", 2012, (color = "black"));
+var car2 = car_info1("Honda", 2020, (color = "black"), (airbags = true));
+var car3 = car_info1(
+  "mercedes",
+  2020,
+  (color = "white"),
+  (airbags = true),
+  (number_of_airbags = 8)
+);
+console.log(car1);
+console.log(car2);
+console.log(car3);
+
+function car_info(manufacturer, model, ...params) {
+  var information = {
+    manufacturer: manufacturer,
+    model: model,
+    params: params,
+  };
+  return information;
+}
+
+var car4 = car_info("Toyota", 2012, (color = "black"), (airbags = false));
+var car5 = car_info("Honda", 2020, (color = "Yellow"));
+var car6 = car_info(
+  "mercedes",
+  2020,
+  (color = "white"),
+  (airbags = true),
+  (number_of_airbags = 8)
+);
+console.log(car4);
+console.log(car5);
+console.log(car6);
